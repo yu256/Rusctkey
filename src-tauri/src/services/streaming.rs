b799c9c0cafccc05372a63fa5d4a348604327914
yu256 <122450013+yu256@users.noreply.github.com>
2023-06-29 23:17:35 +0900

    うごかん thread '<unnamed>' panicked at 'Cannot start a runtime from within a runtime. This happens because a function (like `block_on`) attempted to block the current thread while the thread is being used to drive asynchronous tasks.'

--- src-tauri/src/services/streaming.rs
+++ src-tauri/src/services/streaming.rs
@@ -1,12 +1,50 @@
-use futures::{SinkExt, StreamExt};
+use std::pin::Pin;
+use std::sync::{Arc, Mutex};
+
+use futures::stream::SplitSink;
+use futures::SinkExt;
+use futures::{stream::SplitStream, StreamExt};
+use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use tauri::Manager;
-use tokio_tungstenite::{connect_async, tungstenite::Message};
+use tokio::net::TcpStream;
+use tokio_tungstenite::MaybeTlsStream;
+use tokio_tungstenite::{connect_async, tungstenite::Message, WebSocketStream};
 
 use crate::services::Note;
 
 use super::service::{TOKEN, URL};
+
+struct WebSocketHandler {
+    writer: SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>,
+    reader: SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>,
+}
+
+impl WebSocketHandler {
+    async fn new() -> Self {
+        let url: &str = &URL;
+        let token: &str = &TOKEN;
+        let stream_url = format!("wss://{}/streaming?i={}", url, token);
+
+        let (stream, _) = connect_async(stream_url).await.expect("Failed to connect");
+
+        let (writer, reader) = stream.split();
+
+        WebSocketHandler { writer, reader }
+    }
+}
+
+static WEBSOCKET_HANDLER: Lazy<Arc<Mutex<WebSocketHandler>>> = Lazy::new(|| {
+    let websocket_handler = tokio::runtime::Runtime::new()
+        .unwrap()
+        .block_on(async { WebSocketHandler::new().await });
+    Arc::new(Mutex::new(websocket_handler))
+});
+
+fn get_websocket_handler() -> Arc<Mutex<WebSocketHandler>> {
+    Arc::clone(&*WEBSOCKET_HANDLER)
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 #[allow(non_snake_case)]
@@ -23,13 +61,11 @@ pub struct Body {
 }
 
 pub async fn streaming(app_handle: tauri::AppHandle) {
-    let url: &str = &URL;
-    let token: &str = &TOKEN;
-    let stream_url = format!("wss://{}/streaming?i={}", url, token);
+    let handler = get_websocket_handler();
 
-    let (stream, _) = connect_async(stream_url).await.expect("Failed to connect");
-
-    let (mut write, read) = stream.split();
+    let write = &mut handler.lock().unwrap().writer;
+    let reader = &mut handler.lock().unwrap().reader;
+    let pinned_reader = Pin::new(reader);
 
     write
         .send({
@@ -45,23 +81,24 @@ pub async fn streaming(app_handle: tauri::AppHandle) {
         .await
         .unwrap();
 
-    read.for_each(|message| async {
-        let message = message.unwrap().to_text().unwrap().to_string();
+    pinned_reader
+        .for_each(|message| async {
+            let message = message.unwrap().to_text().unwrap().to_string();
 
-        let mut streaming_body: StreamingBody =
-            match serde_json::from_str::<StreamingBody>(message.as_str()) {
-                Ok(deserialized) => deserialized,
-                Err(error) => {
-                    eprintln!("Error: {}", error);
-                    todo!()
-                }
-            };
+            let mut streaming_body: StreamingBody =
+                match serde_json::from_str::<StreamingBody>(message.as_str()) {
+                    Ok(deserialized) => deserialized,
+                    Err(error) => {
+                        eprintln!("Error: {}", error);
+                        todo!()
+                    }
+                };
 
-        super::note_modifier::modify_notes(&mut streaming_body.body.body).await;
+            super::note_modifier::modify_notes(&mut streaming_body.body.body).await;
 
-        app_handle
-            .emit_all("timeline", &streaming_body.body.body)
-            .unwrap();
-    })
-    .await;
+            app_handle
+                .emit_all("timeline", &streaming_body.body.body)
+                .unwrap();
+        })
+        .await;
 }
